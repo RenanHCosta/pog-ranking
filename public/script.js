69a0de53eeb77b5d154488dc55e97a37a75b3db5
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2023-11-14 23:59:49 -0300

    feat: atualização de estilização no pog ranking

--- public/script.js
+++ public/script.js
@@ -1,98 +1,172 @@
-
-const BASE_API_URL = 'https://pog-ranking.onrender.com'; // PRODUCTION URL
+const BASE_API_URL = "https://pog-ranking.onrender.com"; // PRODUCTION URL
 // const BASE_API_URL = 'http://localhost:3000'; // LOCAL URL
-const SUMMONER_API = 'summoner';
-const LEAGUE_API = 'summoner/league'
+const SUMMONER_API = "summoner";
+const LEAGUE_API = "summoner/league";
 
-const TIER_ORDER = ["IRON", "BRONZE", "SILVER", "GOLD", "PLATINUM", "EMERALD", "DIAMOND", "MASTER", "GRANDMASTER", "CHALLENGER"]
+const TIER_ORDER = [
+  "IRON",
+  "BRONZE",
+  "SILVER",
+  "GOLD",
+  "PLATINUM",
+  "EMERALD",
+  "DIAMOND",
+  "MASTER",
+  "GRANDMASTER",
+  "CHALLENGER",
+];
 
 const RANK_ORDER = ["IV", "III", "II", "I"];
 
 const players = [
-    "Smurf do Nenem",
-    "Kaikan",
-    "Neyans",
-    "SrSSS",
-    "Dreosh",
-    "Ieyasu Sawada",
-    "lele01",
-    "Nekozumi",
-    "Arzok",
-    "eigamen",
-    "laimb meu zovo",
-    "Srpbl",
-    "Brutos MAX",
-    "always apleasure",
-    "Nenemz",
-    "rasks"
+  {
+    name: "Smurf do Nenem",
+    img: "./nenemz.png",
+  },
+  {
+    name: "Kaikan",
+    img: "./kaikan.png",
+  },
+  {
+    name: "Neyans",
+    img: "./neyans.png",
+  },
+  {
+    name: "SrSSS",
+    img: "./srsss.png",
+  },
+  {
+    name: "Dreosh",
+    img: "./dreosh.png",
+  },
+  {
+    name: "Ieyasu Sawada",
+    img: "./gomes.png",
+  },
+  {
+    name: "lele01",
+    img: "./lele01.png",
+  },
+  {
+    name: "Nekozumi",
+    img: "./nunu.png",
+  },
+  {
+    name: "Arzok",
+    img: "./arzok.png",
+  },
+  {
+    name: "eigamen",
+    img: "",
+  },
+  {
+    name: "laimb meu zovo",
+    img: "",
+  },
+  {
+    name: "Srpbl",
+    img: "",
+  },
+  {
+    name: "always apleasure",
+    img: "./kaikan.png",
+  },
+  {
+    name: "Nenemz",
+    img: "./nenemz.png",
+  },
+  {
+    name: "rasks",
+    img: "",
+  },
 ];
 
 async function fetchSummonerByName(summonerName) {
-    const response = await fetch(`${BASE_API_URL}/${SUMMONER_API}/${summonerName}`)
-    const data = await response.json();
-    return data;
+  const response = await fetch(
+    `${BASE_API_URL}/${SUMMONER_API}/${summonerName}`
+  );
+  const data = await response.json();
+  return data;
 }
 
 async function fetchSummonerLeagues(summonerName) {
-    const response = await fetch(`${BASE_API_URL}/${LEAGUE_API}/${summonerName}`)
-    const data = await response.json();
-    return data;
+  const response = await fetch(`${BASE_API_URL}/${LEAGUE_API}/${summonerName}`);
+  const data = await response.json();
+  return data;
 }
 
 async function getSummonersLeagues(summoners) {
-    const fetchPromises = summoners.map(async (summoner) => {
-        const leagueData = await fetchSummonerLeagues(summoner);
-        return { summoner, leagueData };
-    });
+  const fetchPromises = summoners.map(async (summoner) => {
+    const leagueData = await fetchSummonerLeagues(summoner.name);
+    return { summoner, leagueData };
+  });
 
-    const summonersWithLeagues = await Promise.all(fetchPromises);
+  const summonersWithLeagues = await Promise.all(fetchPromises);
 
-    return summonersWithLeagues;
+  return summonersWithLeagues;
 }
 
 function comparePlayers(playerA, playerB) {
-    const tierComparison = TIER_ORDER.indexOf(playerB.tier) - TIER_ORDER.indexOf(playerA.tier);
+  const tierComparison =
+    TIER_ORDER.indexOf(playerB.tier) - TIER_ORDER.indexOf(playerA.tier);
 
-    if (tierComparison === 0) {
-        return RANK_ORDER.indexOf(playerB.rank) - RANK_ORDER.indexOf(playerA.rank);
-    }
+  if (tierComparison === 0) {
+    return RANK_ORDER.indexOf(playerB.rank) - RANK_ORDER.indexOf(playerA.rank);
+  }
 
-    return tierComparison;
+  return tierComparison;
 }
 
 function renderRanking(ranking) {
-    const rankingWrapper = document.querySelector("#ranking");
+  const rankingWrapper = document.querySelector("#ranking");
 
-    let html = '';
+  let html = "";
 
-    const orderedPlayers = ranking.map(summoner => {
-        if (summoner.leagueData.status) return false;
-        const soloQueueData = summoner.leagueData.find(data => data.queueType === 'RANKED_SOLO_5x5');
-        if (soloQueueData) return soloQueueData;
-    }).filter(Boolean).sort(comparePlayers);
-    
-    orderedPlayers.forEach((summoner) => {
-        html += `
+  const orderedPlayers = ranking
+    .map((summoner) => {
+      if (summoner.leagueData.status) return false;
+      const soloQueueData = summoner.leagueData.find(
+        (data) => data.queueType === "RANKED_SOLO_5x5"
+      );
+      if (soloQueueData) return soloQueueData;
+    })
+    .filter(Boolean)
+    .sort(comparePlayers);
+
+  orderedPlayers.forEach((summoner) => {
+    const player = players.find(
+      (player) => player.name === summoner.summonerName
+    );
+    const playerImg = player && player.img ? player.img : "indoali.jpg";
+
+    const negativeClass = summoner.wins < summoner.losses ? "on" : "off";
+
+    html += `
             <div class="summoner">
-                <span>${summoner.summonerName}</span>
+              <div class="playerimg">
+                <img src=${playerImg} />
+                <span class="negative ${negativeClass}">CRIMINOSO</span>
+                <img class="cadeiaimg ${negativeClass}" src="./grade-cadeia.png"/>
+              </div>
                 <div class="content">
-                    <span class="tier ${summoner.tier}">${summoner.tier}</span>
+                    <span>${summoner.summonerName}</span>
+                    <div class="elo"><span class="tier ${summoner.tier}">${summoner.tier}</span>
                     <span class="rank">${summoner.rank}</span>
-                    <span class="points">${summoner.leaguePoints} LP</span>
-                    <span class="wins">${summoner.wins} V</span>
-                    <span class="losses">${summoner.losses} D</span>
+                    <span class="points">${summoner.leaguePoints} LP</span></div>
+                    <span class="wins">${summoner.wins} Vitórias</span>
+                    <span class="losses ${negativeClass}">${summoner.losses} Derrotas</span>
                 </div>
             </div>
         `;
-    })
+  });
 
-    rankingWrapper.innerHTML = html;
+  rankingWrapper.innerHTML = html;
 }
 
 getSummonersLeagues(players)
-    .then((result) => {
-        renderRanking(result);
-    })
-    .catch((error) => {
-        console.error('Erro geral:', error);
-    });
+  .then((result) => {
+    renderRanking(result);
+  })
+  .catch((error) => {
+    console.error("Erro geral:", error);
+  });
